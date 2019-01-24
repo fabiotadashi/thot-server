ce0711d1da0457202292c8fb95d43415d2f680d8
Fabio Miyasato <fabiomiyasato@Fabios-MacBook-Pro.local>
2019-01-24 14:15:25 -0200

    imports

--- src/main/java/io/redspark/thot/config/ThotConfig.java
+++ src/main/java/io/redspark/thot/config/ThotConfig.java
@@ -2,11 +2,8 @@ package io.redspark.thot.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import springfox.documentation.builders.PathSelectors;
