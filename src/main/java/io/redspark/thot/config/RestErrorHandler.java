2182b654a2d9961c736ba7cce954b628a869c85c
Fabio Miyasato <fabiomiyasato@Fabios-MacBook-Pro.local>
2019-01-28 23:27:20 -0200

    error handling

---
+++ src/main/java/io/redspark/thot/config/RestErrorHandler.java
@@ -0,0 +1,54 @@
+package io.redspark.thot.config;
+
+import io.redspark.thot.controller.dto.ValidationErrorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Locale;
+
+@ControllerAdvice
+public class RestErrorHandler {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public RestErrorHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ValidationErrorDTO processValidationError(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        return processFieldErrors(fieldErrors);
+    }
+
+    private ValidationErrorDTO processFieldErrors(List<FieldError> fieldErrors) {
+        ValidationErrorDTO dto = new ValidationErrorDTO();
+        dto.setCode(HttpStatus.BAD_REQUEST.value());
+
+        fieldErrors.stream()
+                .findFirst()
+                .ifPresent(fieldError -> dto.setMessage(resolveLocalizedErrorMessage(fieldError)));
+
+        return dto;
+    }
+
+    private String resolveLocalizedErrorMessage(FieldError fieldError) {
+        Locale currentLocale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(fieldError, currentLocale);
+    }
+}
---
+++ src/main/java/io/redspark/thot/controller/dto/ValidationErrorDTO.java
@@ -0,0 +1,10 @@
+package io.redspark.thot.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class ValidationErrorDTO {
+
+    private Integer code;
+    private String message;
+}
